99ae4a7dd5915218b49ed67e43f94bf170849d48
Sasa Pettyjohn <sasa@Sasas-MacBook-Pro.local>
2023-12-20 11:42:15 -0800

    rename import

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,13 +1,13 @@
 import {
   // @ts-ignore
-  ExposedAppProviders,
-} from '@clearblade/ia-mfe-react';
-import DashboardPlugin from './components/DashboardPlugin';
+  AppProviders,
+} from "@clearblade/ia-mfe-react";
+import DashboardPlugin from "./components/DashboardPlugin";
 
 export default function Root() {
   return (
-    <ExposedAppProviders>
+    <AppProviders>
       <DashboardPlugin />
-    </ExposedAppProviders>
+    </AppProviders>
   );
 }
